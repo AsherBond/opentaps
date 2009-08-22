8bdbc21d5c14dcfbcd5dc873fb5a40597d1e0373
sparksun <sparksun@d3523486-f5fe-0310-a41a-a15a4e76f3c7>
2009-08-22 02:04:34 +0000

    #1080 if transEntries is empty then not call createAcctgTransAndEntries, else will get exception
    git-svn-id: svn://svn.opentaps.org/opentaps_all/branches/ofbiz-0904-1.4.0-preview1@13150 d3523486-f5fe-0310-a41a-a15a4e76f3c7

--- hot-deploy/financials/src/com/opensourcestrategies/financials/ledger/LedgerServices.java
+++ hot-deploy/financials/src/com/opensourcestrategies/financials/ledger/LedgerServices.java
@@ -2718,21 +2718,21 @@ public final class LedgerServices {
                     }
                 }
             }
-            // Perform the transaction
-            input = UtilMisc.toMap("transactionDate", UtilDateTime.nowTimestamp(), "userLogin", userLogin);
-            input.put("acctgTransEntries", transEntries);
-            input.put("glFiscalTypeId", "ACTUAL");
-            input.put("acctgTransTypeId", "MANUFACTURING_ATX");
-            input.put("workEffortId", workEffortId);
-
-            Map servResult = dispatcher.runSync("createAcctgTransAndEntries", input);
-
-            if (ServiceUtil.isError(servResult)) {
-                return servResult;
+            // if transEntries is empty, then return directly, else createAcctgTransAndEntries will throw exception
+            if (UtilValidate.isNotEmpty(transEntries)) {
+                // Perform the transaction
+                input = UtilMisc.toMap("transactionDate", UtilDateTime.nowTimestamp(), "userLogin", userLogin);
+                input.put("acctgTransEntries", transEntries);
+                input.put("glFiscalTypeId", "ACTUAL");
+                input.put("acctgTransTypeId", "MANUFACTURING_ATX");
+                input.put("workEffortId", workEffortId);
+    
+                Map servResult = dispatcher.runSync("createAcctgTransAndEntries", input);
+    
+                if (ServiceUtil.isError(servResult)) {
+                    return servResult;
+                }
             }
-
-            Map result = ServiceUtil.returnSuccess();
-            result.put("acctgTransId", servResult.get("acctgTransId"));
 
         } catch (GenericEntityException ex) {
             return ServiceUtil.returnError(ex.getMessage());
