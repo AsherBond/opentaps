18d33e8575a29fb39f548bbca2c5dc9336aa8b9c
sparksun <sparksun@d3523486-f5fe-0310-a41a-a15a4e76f3c7>
2009-08-23 02:49:17 +0000

    #1080 only if transEntries not empty then call createAcctgTransAndEntries
    git-svn-id: svn://svn.opentaps.org/opentaps_all/branches/ofbiz-0904-1.4.0-preview1@13151 d3523486-f5fe-0310-a41a-a15a4e76f3c7

--- hot-deploy/financials/src/com/opensourcestrategies/financials/ledger/LedgerServices.java
+++ hot-deploy/financials/src/com/opensourcestrategies/financials/ledger/LedgerServices.java
@@ -2818,21 +2818,20 @@ public final class LedgerServices {
                 }
             }
             // Perform the transaction
-            input = UtilMisc.toMap("transactionDate", UtilDateTime.nowTimestamp(), "userLogin", userLogin);
-            input.put("acctgTransEntries", transEntries);
-            input.put("glFiscalTypeId", "ACTUAL");
-            input.put("acctgTransTypeId", "MANUFACTURING_ATX");
-            input.put("workEffortId", workEffortId);
-
-            Map servResult = dispatcher.runSync("createAcctgTransAndEntries", input);
-
-            if (ServiceUtil.isError(servResult)) {
-                return servResult;
+            // if transEntries is empty, then return directly, else createAcctgTransAndEntries will throw exception
+            if (UtilValidate.isNotEmpty(transEntries)) {
+                input = UtilMisc.toMap("transactionDate", UtilDateTime.nowTimestamp(), "userLogin", userLogin);
+                input.put("acctgTransEntries", transEntries);
+                input.put("glFiscalTypeId", "ACTUAL");
+                input.put("acctgTransTypeId", "MANUFACTURING_ATX");
+                input.put("workEffortId", workEffortId);
+    
+                Map servResult = dispatcher.runSync("createAcctgTransAndEntries", input);
+    
+                if (ServiceUtil.isError(servResult)) {
+                    return servResult;
+                }
             }
-
-            Map result = ServiceUtil.returnSuccess();
-            result.put("acctgTransId", servResult.get("acctgTransId"));
-
         } catch (GeneralException ex) {
             return ServiceUtil.returnError(ex.getMessage());
         }
