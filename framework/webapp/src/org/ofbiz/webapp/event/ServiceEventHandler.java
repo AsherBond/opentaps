b85b653aff76ff3eef8860dd20931082b192cd61
sichen <sichen@d3523486-f5fe-0310-a41a-a15a4e76f3c7>
2009-09-16 16:40:26 +0000

    shorten error message for secured URL call
    git-svn-id: svn://svn.opentaps.org/opentaps_all/branches/ofbiz-0904-1.4.0-preview1@13357 d3523486-f5fe-0310-a41a-a15a4e76f3c7

--- framework/webapp/src/org/ofbiz/webapp/event/ServiceEventHandler.java
+++ framework/webapp/src/org/ofbiz/webapp/event/ServiceEventHandler.java
@@ -395,14 +395,9 @@ public class ServiceEventHandler implements EventHandler {
         // NOTE: the RequestHandler will check the HttpSerletRequest security to make sure it is secure if the request-map -> security -> https=true, but we can't just look at the request.isSecure() method here because it is allowed to send secure requests for request-map with https=false
         if (requestMap != null && requestMap.securityHttps) {
             if (urlOnlyParameterNames.contains(name)) {
-                String errMsg = "Found URL parameter [" + name + "] passed to secure (https) request-map with uri ["
-                    + requestMap.uri + "] with an event that calls service ["
-                    + serviceName + "]; this is not allowed for security reasons! The data should be encrypted by making it part of the request body "
-                    + "(a form field) instead of the request URL."
-                    + " Moreover it would be kind if you could create a Jira sub-task of https://issues.apache.org/jira/browse/OFBIZ-2330 "
-                    + "(check before if a sub-task for this error does not exist)."
-                    + " If you are not sure how to create a Jira issue please have a look before at http://docs.ofbiz.org/x/r."
-                    + " Thank you in advance for your help.";
+                String errMsg = "URL parameter [" + name + "] cannot be passed to secure (https) request-map with uri ["
+                    + requestMap.uri + "] to call service ["
+                    + serviceName + "]. This is not allowed for security reasons."
                 Debug.logError("=============== " + errMsg + "; In session [" + session.getId() + "]; Note that this can be changed using the service.http.parameters.require.encrypted property in the url.properties file", module);
 
                 // the default here is true, so anything but N/n is true
