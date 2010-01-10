e2b8a8b9b139a66fd1a6104f891fec7aa61cdc65
sparksun <sparksun@d3523486-f5fe-0310-a41a-a15a4e76f3c7>
2010-01-10 14:52:22 +0000

    refresh hibernate configuration to fix the exception of not found mapping class
    git-svn-id: svn://svn.opentaps.org/opentaps_all/versions/1.4/trunk@14537 d3523486-f5fe-0310-a41a-a15a4e76f3c7

--- hot-deploy/opentaps-common/src/base/org/opentaps/base/entities/SequenceValueItem.java
+++ hot-deploy/opentaps-common/src/base/org/opentaps/base/entities/SequenceValueItem.java
@@ -69,7 +69,7 @@ fieldMapColumns.put("SequenceValueItem", fields);
     lastUpdatedTxStamp("lastUpdatedTxStamp"),
     createdStamp("createdStamp"),
     createdTxStamp("createdTxStamp");
-    private final String fieldName;
+    protected final String fieldName;
     private Fields(String name) { fieldName = name; }
     /** {@inheritDoc} */
     public String getName() { return fieldName; }
@@ -81,17 +81,17 @@ fieldMapColumns.put("SequenceValueItem", fields);
 
    @Id
    @Column(name="SEQ_NAME")
-   private String seqName;
+   protected String seqName;
    @Column(name="SEQ_ID")
-   private Long seqId;
+   protected Long seqId;
    @Column(name="LAST_UPDATED_STAMP")
-   private Timestamp lastUpdatedStamp;
+   protected Timestamp lastUpdatedStamp;
    @Column(name="LAST_UPDATED_TX_STAMP")
-   private Timestamp lastUpdatedTxStamp;
+   protected Timestamp lastUpdatedTxStamp;
    @Column(name="CREATED_STAMP")
-   private Timestamp createdStamp;
+   protected Timestamp createdStamp;
    @Column(name="CREATED_TX_STAMP")
-   private Timestamp createdTxStamp;
+   protected Timestamp createdTxStamp;
 
   /**
    * Default constructor.
