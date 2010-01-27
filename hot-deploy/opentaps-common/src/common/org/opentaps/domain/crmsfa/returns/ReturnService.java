acb1cbd17e3793856e4200d1d6861fef98568e89
jwickers <jwickers@d3523486-f5fe-0310-a41a-a15a4e76f3c7>
2010-01-27 07:14:42 +0000

    #1357 Cleanup
    git-svn-id: svn://svn.opentaps.org/opentaps_all/versions/1.4/trunk@14765 d3523486-f5fe-0310-a41a-a15a4e76f3c7

--- hot-deploy/opentaps-common/src/common/org/opentaps/domain/crmsfa/returns/ReturnService.java
+++ hot-deploy/opentaps-common/src/common/org/opentaps/domain/crmsfa/returns/ReturnService.java
@@ -38,8 +38,8 @@ import org.opentaps.foundation.service.ServiceException;
 public class ReturnService extends DomainService {
     private String returnItemResponseId = null;
     private String invoiceId;
-	private String invoiceItemSeqId;
-    
+    private String invoiceItemSeqId;
+
     private static final String MODULE = ReturnService.class.getName();
 
 
@@ -49,7 +49,7 @@ public class ReturnService extends DomainService {
     public ReturnService() {
         super();
     }
-    
+
     /**
      * Sets the return item response ID.
      * @param returnItemResponseId the return item response ID.
@@ -79,101 +79,101 @@ public class ReturnService extends DomainService {
      * @throws ServiceException if an error occurs
      */
     public void updatePaymentApplicationAccountTagsByReturnOrder() throws ServiceException {
-    	try {
-			PaymentRepositoryInterface paymentRepository = getDomainsDirectory().getBillingDomain().getPaymentRepository();
-			OrderRepositoryInterface orderRepository = getDomainsDirectory().getOrderDomain().getOrderRepository();
-			ReturnItemResponse returnItemResponse = orderRepository.getReturnItemResponseById(returnItemResponseId);
-			Payment payment = returnItemResponse.getPayment();
-			Debug.logInfo("getPaymentTypeId : " + payment.getPaymentTypeId()
-					+ ", payment.getOrderPaymentPreference() : " + payment.getOrderPaymentPreference(), MODULE);
-			if ("CUSTOMER_REFUND".equals(payment.getPaymentTypeId())) {
-				for (PaymentApplication paymentApplication : payment.getPaymentApplications()) {
-					OrderItem orderItem = null;
-					if (UtilValidate.isNotEmpty(payment.getOrderPaymentPreference())) {
-						orderItem = payment.getOrderPaymentPreference().getOrderItem();
-						Debug.logInfo("orderItem : " + orderItem, MODULE);
-					}
-					// gets order item with relate invoice
-					if (UtilValidate.isEmpty(orderItem) && UtilValidate.isNotEmpty(paymentApplication.getInvoice())) {
-						for (InvoiceItem invoiceItem : paymentApplication.getInvoice().getInvoiceItems()) {
-							if (UtilValidate.isNotEmpty(invoiceItem.getReturnItemBillings().size())) {
-								ReturnItemBilling returnItemBilling = invoiceItem.getReturnItemBillings().get(0);
-								orderItem = returnItemBilling.getReturnItem().getOrderItem();
-								break;
-							}
-						}
-					}
-					Debug.logInfo("orderItem : " + orderItem, MODULE);
-					// copies all the accounting tags from the return order item to the payment application.
-					if (UtilValidate.isNotEmpty(orderItem)) {
-						paymentApplication.setAcctgTagEnumId1(orderItem.getAcctgTagEnumId1());
-						paymentApplication.setAcctgTagEnumId2(orderItem.getAcctgTagEnumId2());
-		                paymentApplication.setAcctgTagEnumId3(orderItem.getAcctgTagEnumId3());
-		                paymentApplication.setAcctgTagEnumId4(orderItem.getAcctgTagEnumId4());
-		                paymentApplication.setAcctgTagEnumId5(orderItem.getAcctgTagEnumId5());
-		                paymentApplication.setAcctgTagEnumId6(orderItem.getAcctgTagEnumId6());
-		                paymentApplication.setAcctgTagEnumId7(orderItem.getAcctgTagEnumId7());
-		                paymentApplication.setAcctgTagEnumId8(orderItem.getAcctgTagEnumId8());
-		                paymentApplication.setAcctgTagEnumId9(orderItem.getAcctgTagEnumId9());
-		                paymentApplication.setAcctgTagEnumId10(orderItem.getAcctgTagEnumId10());
-		                paymentRepository.update(paymentApplication);
+        try {
+            PaymentRepositoryInterface paymentRepository = getDomainsDirectory().getBillingDomain().getPaymentRepository();
+            OrderRepositoryInterface orderRepository = getDomainsDirectory().getOrderDomain().getOrderRepository();
+            ReturnItemResponse returnItemResponse = orderRepository.getReturnItemResponseById(returnItemResponseId);
+            Payment payment = returnItemResponse.getPayment();
+            Debug.logInfo("getPaymentTypeId : " + payment.getPaymentTypeId()
+                          + ", payment.getOrderPaymentPreference() : " + payment.getOrderPaymentPreference(), MODULE);
+            if ("CUSTOMER_REFUND".equals(payment.getPaymentTypeId())) {
+                for (PaymentApplication paymentApplication : payment.getPaymentApplications()) {
+                    OrderItem orderItem = null;
+                    if (UtilValidate.isNotEmpty(payment.getOrderPaymentPreference())) {
+                        orderItem = payment.getOrderPaymentPreference().getOrderItem();
+                        Debug.logInfo("orderItem : " + orderItem, MODULE);
+                    }
+                    // gets order item with relate invoice
+                    if (UtilValidate.isEmpty(orderItem) && UtilValidate.isNotEmpty(paymentApplication.getInvoice())) {
+                        for (InvoiceItem invoiceItem : paymentApplication.getInvoice().getInvoiceItems()) {
+                            if (UtilValidate.isNotEmpty(invoiceItem.getReturnItemBillings().size())) {
+                                ReturnItemBilling returnItemBilling = invoiceItem.getReturnItemBillings().get(0);
+                                orderItem = returnItemBilling.getReturnItem().getOrderItem();
+                                break;
+                            }
+                        }
+                    }
+                    Debug.logInfo("orderItem : " + orderItem, MODULE);
+                    // copies all the accounting tags from the return order item to the payment application.
+                    if (UtilValidate.isNotEmpty(orderItem)) {
+                        paymentApplication.setAcctgTagEnumId1(orderItem.getAcctgTagEnumId1());
+                        paymentApplication.setAcctgTagEnumId2(orderItem.getAcctgTagEnumId2());
+                        paymentApplication.setAcctgTagEnumId3(orderItem.getAcctgTagEnumId3());
+                        paymentApplication.setAcctgTagEnumId4(orderItem.getAcctgTagEnumId4());
+                        paymentApplication.setAcctgTagEnumId5(orderItem.getAcctgTagEnumId5());
+                        paymentApplication.setAcctgTagEnumId6(orderItem.getAcctgTagEnumId6());
+                        paymentApplication.setAcctgTagEnumId7(orderItem.getAcctgTagEnumId7());
+                        paymentApplication.setAcctgTagEnumId8(orderItem.getAcctgTagEnumId8());
+                        paymentApplication.setAcctgTagEnumId9(orderItem.getAcctgTagEnumId9());
+                        paymentApplication.setAcctgTagEnumId10(orderItem.getAcctgTagEnumId10());
+                        paymentRepository.update(paymentApplication);
 
-		                //update payment accounting tags as well
-		                payment.setAcctgTagEnumId1(orderItem.getAcctgTagEnumId1());
-		                payment.setAcctgTagEnumId2(orderItem.getAcctgTagEnumId2());
-		                payment.setAcctgTagEnumId3(orderItem.getAcctgTagEnumId3());
-		                payment.setAcctgTagEnumId4(orderItem.getAcctgTagEnumId4());
-		                payment.setAcctgTagEnumId5(orderItem.getAcctgTagEnumId5());
-		                payment.setAcctgTagEnumId6(orderItem.getAcctgTagEnumId6());
-		                payment.setAcctgTagEnumId7(orderItem.getAcctgTagEnumId7());
-		                payment.setAcctgTagEnumId8(orderItem.getAcctgTagEnumId8());
-		                payment.setAcctgTagEnumId9(orderItem.getAcctgTagEnumId9());
-		                payment.setAcctgTagEnumId10(orderItem.getAcctgTagEnumId10());
-		                paymentRepository.update(payment);
-					}
-				}
-			}
-			
-		} catch (RepositoryException e) {
-			throw new ServiceException(e);
-		} catch (EntityNotFoundException e) {
-			throw new ServiceException(e);
-		}
+                        //update payment accounting tags as well
+                        payment.setAcctgTagEnumId1(orderItem.getAcctgTagEnumId1());
+                        payment.setAcctgTagEnumId2(orderItem.getAcctgTagEnumId2());
+                        payment.setAcctgTagEnumId3(orderItem.getAcctgTagEnumId3());
+                        payment.setAcctgTagEnumId4(orderItem.getAcctgTagEnumId4());
+                        payment.setAcctgTagEnumId5(orderItem.getAcctgTagEnumId5());
+                        payment.setAcctgTagEnumId6(orderItem.getAcctgTagEnumId6());
+                        payment.setAcctgTagEnumId7(orderItem.getAcctgTagEnumId7());
+                        payment.setAcctgTagEnumId8(orderItem.getAcctgTagEnumId8());
+                        payment.setAcctgTagEnumId9(orderItem.getAcctgTagEnumId9());
+                        payment.setAcctgTagEnumId10(orderItem.getAcctgTagEnumId10());
+                        paymentRepository.update(payment);
+                    }
+                }
+            }
+
+        } catch (RepositoryException e) {
+            throw new ServiceException(e);
+        } catch (EntityNotFoundException e) {
+            throw new ServiceException(e);
+        }
     }
-    
+
     /**
      * Copies all the accounting tags from the return order item to the invoice item.
      * @throws ServiceException if an error occurs
      */
     public void updateInvoiceItemAccountTagsByReturnOrder() throws ServiceException {
-    	try {
-			InvoiceRepositoryInterface invoiceRepository = getDomainsDirectory().getBillingDomain().getInvoiceRepository();
-			InvoiceItem invoiceItem = invoiceRepository.getInvoiceItemById(invoiceId, invoiceItemSeqId);
-			// if the invoice type equals CUST_RTN_INVOICE and the invoice item have relate return item billing entities
-			// then copies all the accounting tags from the return order item to the invoice item.
-			if ("CUST_RTN_INVOICE".equals(invoiceItem.getInvoice().getInvoiceTypeId()) && invoiceItem.getReturnItemBillings().size() > 0) {
-				ReturnItemBilling returnItemBilling = invoiceItem.getReturnItemBillings().get(0);
-				OrderItem orderItem = returnItemBilling.getReturnItem().getOrderItem();
-				if (UtilValidate.isNotEmpty(orderItem)) {
-	                invoiceItem.setAcctgTagEnumId1(orderItem.getAcctgTagEnumId1());
-	                invoiceItem.setAcctgTagEnumId2(orderItem.getAcctgTagEnumId2());
-	                invoiceItem.setAcctgTagEnumId3(orderItem.getAcctgTagEnumId3());
-	                invoiceItem.setAcctgTagEnumId4(orderItem.getAcctgTagEnumId4());
-	                invoiceItem.setAcctgTagEnumId5(orderItem.getAcctgTagEnumId5());
-	                invoiceItem.setAcctgTagEnumId6(orderItem.getAcctgTagEnumId6());
-	                invoiceItem.setAcctgTagEnumId7(orderItem.getAcctgTagEnumId7());
-	                invoiceItem.setAcctgTagEnumId8(orderItem.getAcctgTagEnumId8());
-	                invoiceItem.setAcctgTagEnumId9(orderItem.getAcctgTagEnumId9());
-	                invoiceItem.setAcctgTagEnumId10(orderItem.getAcctgTagEnumId10());
-	                invoiceRepository.update(invoiceItem);
-				}
-			}
-			
-		} catch (RepositoryException e) {
-			throw new ServiceException(e);
-		} catch (EntityNotFoundException e) {
-			throw new ServiceException(e);
-		}
+        try {
+            InvoiceRepositoryInterface invoiceRepository = getDomainsDirectory().getBillingDomain().getInvoiceRepository();
+            InvoiceItem invoiceItem = invoiceRepository.getInvoiceItemById(invoiceId, invoiceItemSeqId);
+            // if the invoice type equals CUST_RTN_INVOICE and the invoice item have relate return item billing entities
+            // then copies all the accounting tags from the return order item to the invoice item.
+            if ("CUST_RTN_INVOICE".equals(invoiceItem.getInvoice().getInvoiceTypeId()) && invoiceItem.getReturnItemBillings().size() > 0) {
+                ReturnItemBilling returnItemBilling = invoiceItem.getReturnItemBillings().get(0);
+                OrderItem orderItem = returnItemBilling.getReturnItem().getOrderItem();
+                if (UtilValidate.isNotEmpty(orderItem)) {
+                    invoiceItem.setAcctgTagEnumId1(orderItem.getAcctgTagEnumId1());
+                    invoiceItem.setAcctgTagEnumId2(orderItem.getAcctgTagEnumId2());
+                    invoiceItem.setAcctgTagEnumId3(orderItem.getAcctgTagEnumId3());
+                    invoiceItem.setAcctgTagEnumId4(orderItem.getAcctgTagEnumId4());
+                    invoiceItem.setAcctgTagEnumId5(orderItem.getAcctgTagEnumId5());
+                    invoiceItem.setAcctgTagEnumId6(orderItem.getAcctgTagEnumId6());
+                    invoiceItem.setAcctgTagEnumId7(orderItem.getAcctgTagEnumId7());
+                    invoiceItem.setAcctgTagEnumId8(orderItem.getAcctgTagEnumId8());
+                    invoiceItem.setAcctgTagEnumId9(orderItem.getAcctgTagEnumId9());
+                    invoiceItem.setAcctgTagEnumId10(orderItem.getAcctgTagEnumId10());
+                    invoiceRepository.update(invoiceItem);
+                }
+            }
+
+        } catch (RepositoryException e) {
+            throw new ServiceException(e);
+        } catch (EntityNotFoundException e) {
+            throw new ServiceException(e);
+        }
     }
 
 
