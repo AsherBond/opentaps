88ce668266ae11bc6d1b178609bba52b19911e5c
jwickers <jwickers@d3523486-f5fe-0310-a41a-a15a4e76f3c7>
2010-01-28 13:09:21 +0000

    #1357 Replaced the reimplementation of the ofbiz service by a seca
    git-svn-id: svn://svn.opentaps.org/opentaps_all/versions/1.4/trunk@14792 d3523486-f5fe-0310-a41a-a15a4e76f3c7

--- hot-deploy/opentaps-common/src/common/org/opentaps/domain/crmsfa/returns/ReturnService.java
+++ hot-deploy/opentaps-common/src/common/org/opentaps/domain/crmsfa/returns/ReturnService.java
@@ -16,6 +16,9 @@
  */
 package org.opentaps.domain.crmsfa.returns;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilValidate;
 import org.opentaps.base.entities.InvoiceItem;
@@ -25,6 +28,7 @@ import org.opentaps.base.entities.PaymentApplication;
 import org.opentaps.base.entities.ReturnItemBilling;
 import org.opentaps.base.entities.ReturnItemResponse;
 import org.opentaps.domain.DomainService;
+import org.opentaps.domain.billing.invoice.Invoice;
 import org.opentaps.domain.billing.invoice.InvoiceRepositoryInterface;
 import org.opentaps.domain.billing.payment.PaymentRepositoryInterface;
 import org.opentaps.domain.order.OrderRepositoryInterface;
@@ -37,6 +41,7 @@ import org.opentaps.foundation.service.ServiceException;
  */
 public class ReturnService extends DomainService {
     private String returnItemResponseId = null;
+    private String invoiceId;
 
     private static final String MODULE = ReturnService.class.getName();
 
@@ -57,7 +62,16 @@ public class ReturnService extends DomainService {
     }
 
     /**
+     * Sets the invoice ID.
+     * @param invoiceId the invoice ID
+     */
+    public void setInvoiceId(String invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    /**
      * Copies all the accounting tags from the return order item to the payment application.
+     * This is called as a SECA on createPaymentApplicationsFromReturnItemResponse.
      * @throws ServiceException if an error occurs
      */
     public void updatePaymentApplicationAccountTagsByReturnOrder() throws ServiceException {
@@ -114,6 +128,81 @@ public class ReturnService extends DomainService {
                         paymentRepository.update(payment);
                     }
                 }
+            }
+
+        } catch (RepositoryException e) {
+            throw new ServiceException(e);
+        } catch (EntityNotFoundException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    /**
+     * Copies all the accounting tags from the return order items to the invoice items.
+     * This is called as a SECA on createInvoiceFromReturn.
+     * @throws ServiceException if an error occurs
+     */
+    public void updateInvoiceItemsAccountingTagsForReturn() throws ServiceException {
+        try {
+            // get the invoice that was created from the return
+            InvoiceRepositoryInterface invoiceRepository = getDomainsDirectory().getBillingDomain().getInvoiceRepository();
+            Invoice invoice = invoiceRepository.getInvoiceById(invoiceId);
+            List<? extends InvoiceItem> items = invoice.getInvoiceItems();
+
+            // the first order item found will be used to tag the invoice items that have no related order item
+            // for example the global adjustments on the invoice such as Shipping charges.
+            OrderItem firstOrderItem = null;
+            // we process those order items in a second pass
+            List<InvoiceItem> noOrderItems = new ArrayList<InvoiceItem>();
+
+            for (InvoiceItem invoiceItem : items) {
+                List<? extends ReturnItemBilling> billings = invoiceItem.getReturnItemBillings();
+                // one invoice item has only one related return item if any
+                if (UtilValidate.isNotEmpty(billings)) {
+                    ReturnItemBilling billing = billings.get(0);
+                    OrderItem orderItem = billing.getReturnItem().getOrderItem();
+                    if (orderItem != null) {
+                        if (firstOrderItem == null) {
+                            firstOrderItem = orderItem;
+                        }
+                        invoiceItem.setAcctgTagEnumId1(orderItem.getAcctgTagEnumId1());
+                        invoiceItem.setAcctgTagEnumId2(orderItem.getAcctgTagEnumId2());
+                        invoiceItem.setAcctgTagEnumId3(orderItem.getAcctgTagEnumId3());
+                        invoiceItem.setAcctgTagEnumId4(orderItem.getAcctgTagEnumId4());
+                        invoiceItem.setAcctgTagEnumId5(orderItem.getAcctgTagEnumId5());
+                        invoiceItem.setAcctgTagEnumId6(orderItem.getAcctgTagEnumId6());
+                        invoiceItem.setAcctgTagEnumId7(orderItem.getAcctgTagEnumId7());
+                        invoiceItem.setAcctgTagEnumId8(orderItem.getAcctgTagEnumId8());
+                        invoiceItem.setAcctgTagEnumId9(orderItem.getAcctgTagEnumId9());
+                        invoiceItem.setAcctgTagEnumId10(orderItem.getAcctgTagEnumId10());
+                        invoiceRepository.update(invoiceItem);
+                        continue;
+                    }
+                }
+                // if we are here then no order item was found related to the invoice item
+                noOrderItems.add(invoiceItem);
+            }
+
+            // second pass for the invoice items we could not tag earlier
+            if (firstOrderItem != null) {
+                for (InvoiceItem invoiceItem : noOrderItems) {
+                    invoiceItem.setAcctgTagEnumId1(firstOrderItem.getAcctgTagEnumId1());
+                    invoiceItem.setAcctgTagEnumId2(firstOrderItem.getAcctgTagEnumId2());
+                    invoiceItem.setAcctgTagEnumId3(firstOrderItem.getAcctgTagEnumId3());
+                    invoiceItem.setAcctgTagEnumId4(firstOrderItem.getAcctgTagEnumId4());
+                    invoiceItem.setAcctgTagEnumId5(firstOrderItem.getAcctgTagEnumId5());
+                    invoiceItem.setAcctgTagEnumId6(firstOrderItem.getAcctgTagEnumId6());
+                    invoiceItem.setAcctgTagEnumId7(firstOrderItem.getAcctgTagEnumId7());
+                    invoiceItem.setAcctgTagEnumId8(firstOrderItem.getAcctgTagEnumId8());
+                    invoiceItem.setAcctgTagEnumId9(firstOrderItem.getAcctgTagEnumId9());
+                    invoiceItem.setAcctgTagEnumId10(firstOrderItem.getAcctgTagEnumId10());
+                    invoiceRepository.update(invoiceItem);
+                }
+            } else {
+                // this should not happen as it is not possible to create a return
+                // without an item from the order (eg: just refunding an adjustment)
+                // log this as an error
+                Debug.logError("updateInvoiceItemsAccountingTagsForReturn: did not find any related order item to take the accounting tags from for Return invoice [" + invoiceId + "]", MODULE);
             }
 
         } catch (RepositoryException e) {
