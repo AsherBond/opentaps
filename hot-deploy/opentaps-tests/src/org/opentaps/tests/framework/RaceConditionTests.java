e24fe7c1254a2d3693ec1123b0cce596570f618f
jwickers <jwickers@d3523486-f5fe-0310-a41a-a15a4e76f3c7>
2010-03-16 19:31:46 +0000

    #1460 Add test for race condition bug in getNextOrderId
    See JIRA https://issues.apache.org/jira/browse/OFBIZ-3557
    
    git-svn-id: svn://svn.opentaps.org/opentaps_all/versions/1.4/trunk@15184 d3523486-f5fe-0310-a41a-a15a4e76f3c7

--- hot-deploy/opentaps-tests/src/org/opentaps/tests/framework/RaceConditionTests.java
+++ hot-deploy/opentaps-tests/src/org/opentaps/tests/framework/RaceConditionTests.java
@@ -17,8 +17,11 @@
 package org.opentaps.tests.framework;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.GeneralException;
+import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
 import org.opentaps.tests.OpentapsTestCase;
@@ -145,6 +148,115 @@ public class RaceConditionTests extends OpentapsTestCase {
             try {
                 detail.create();
             } catch (GenericEntityException ge) {
+                this.e = ge;
+                finished = true;
+                return;
+            }
+
+            finished = true;
+            success = true;
+        }
+    }
+
+
+    /**
+     * Tests the "getNextOrderId" service when using ODRSQ_ENF_SEQ (Enforce sequence), in that configuration
+     * the service must return ids in sequence without gap.
+     * This must work if two users are creating orders concurrently (no order ID collision).
+     * The same applies to other services generating IDs in sequence, eg: for Invoices / Quotes / ...
+     * @exception Exception if an error occurs
+     */
+    public void testNextOrderId() throws Exception {
+
+        // get the partyAcctgPreference current setting
+        GenericValue partyAcctgPreference = delegator.findByPrimaryKey("PartyAcctgPreference", UtilMisc.toMap("partyId", organizationPartyId));
+        assertNotNull("Failed to get the partyAcctgPreference for " + organizationPartyId, partyAcctgPreference);
+        String orderSequenceEnumId = partyAcctgPreference.getString("orderSequenceEnumId");
+        Long lastOrderNumber = partyAcctgPreference.getLong("lastOrderNumber");
+
+        // set ODRSQ_ENF_SEQ
+        partyAcctgPreference.set("orderSequenceEnumId", "ODRSQ_ENF_SEQ");
+        delegator.store(partyAcctgPreference);
+
+        final NextOrderIdThread t1;
+        final NextOrderIdThread t2;
+
+        try {
+
+            // create our special thread objects, which suspend themselves right after calling getNextOrderId
+            t1 = new NextOrderIdThread();
+            t2 = new NextOrderIdThread();
+            t1.start();
+            // wait 2 sec before starting the order thread
+            Thread.sleep(2000);
+            t2.start();
+
+            // wait until both thread completed
+            while (!t1.isFinished()) { Thread.sleep(1000); Debug.logInfo("Waiting for NextOrderIdThread 1 ...", MODULE); }
+            while (!t2.isFinished()) { Thread.sleep(1000); Debug.logInfo("Waiting for NextOrderIdThread 2 ...", MODULE); }
+        } finally {
+            partyAcctgPreference = delegator.findByPrimaryKey("PartyAcctgPreference", UtilMisc.toMap("partyId", organizationPartyId));
+            partyAcctgPreference.set("orderSequenceEnumId", orderSequenceEnumId);
+            delegator.store(partyAcctgPreference);
+        }
+
+        // if we were not successful, then fail with the exception
+        if (!t1.success) {
+            if (t1.e != null) {
+                throw t1.e;
+            }
+            fail("Thread 1 did not succeed as expected for unknown reason.  No exception thrown.");
+        }
+
+        if (!t2.success) {
+            if (t2.e == null) {
+                fail("Thread 2 did not succeed as expected for unknown reason.  No exception thrown.");
+            }
+            if (t2.e instanceof GenericEntityException) {
+                fail("GenericEntityExeption encountered, this is probably a race condition.  Check if following message contains PK violation on inventoryItemDetailSeqId:\n " + t2.e.getMessage());
+            } else {
+                throw t2.e;
+            }
+        }
+
+        // get the returned orderIds
+        String orderId1 = t1.orderId;
+        String orderId2 = t2.orderId;
+
+        Debug.logInfo("Results, from lastOrderNumber [" + lastOrderNumber + "] got " + orderId1 + " and " + orderId2, MODULE);
+
+        // check for collision
+        assertNotEquals("Order ids must not collide.", orderId1, orderId2);
+
+        // check the sequence is respected
+        assertEquals("First order ID not in sequence according to lastOrderNumber: " + lastOrderNumber, orderId1, Long.toString(lastOrderNumber + 1));
+        assertEquals("Second order ID not in sequence according to lastOrderNumber: " + lastOrderNumber, orderId2, Long.toString(lastOrderNumber + 2));
+    }
+
+    /**
+     * Special thread object to force a conflict in getting the next order ID.
+     */
+    class NextOrderIdThread extends Thread {
+        protected boolean halted = true;
+        protected boolean finished = false;
+        protected String orderId;
+
+        public boolean success = false;
+        public Exception e = null;
+        public String inventoryItemDetailSeqId = null;
+
+        public NextOrderIdThread() {
+        }
+
+        public boolean isFinished() { return finished; }
+        public void resumeTest() { synchronized (this) { halted = false; } }
+
+        @Override public void run() {
+            try {
+                Map<String, Object> input = UtilMisc.<String, Object>toMap("userLogin", admin, "partyId", organizationPartyId);
+                Map<String, Object> results = dispatcher.runSync("opentaps.testNextOrderId", input);
+                orderId = (String) results.get("orderId");
+            } catch (GeneralException ge) {
                 this.e = ge;
                 finished = true;
                 return;
---
+++ hot-deploy/opentaps-tests/src/org/opentaps/tests/service/TestNextOrderId.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2010 - 2010 Open Source Strategies, Inc.
+ *
+ * Opentaps is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Opentaps is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Opentaps.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.opentaps.tests.service;
+
+import org.opentaps.base.services.GetNextOrderIdService;
+import org.opentaps.foundation.service.Service;
+import org.opentaps.foundation.service.ServiceException;
+
+/**
+ * A wrapper to getNextOrderId which sleeps to simulate load in order to test concurrenc.
+ */
+public class TestNextOrderId extends Service {
+
+    private String partyId;
+    private String productStoreId;
+
+    private String orderId;
+
+    /**
+     * Gets the orderId returned by the "getNextOrderId" service.
+     * @return the orderId
+     */
+    public String getOrderId() {
+        return orderId;
+    }
+
+    /**
+     * Sets the partyId.
+     * @param partyId the partyId
+     */
+    public void setPartyId(String partyId) {
+        this.partyId = partyId;
+    }
+
+    /**
+     * Sets the productStoreId.
+     * @param productStoreId the productStoreId
+     */
+    public void setProductStoreId(String productStoreId) {
+        this.productStoreId = productStoreId;
+    }
+
+
+    /**
+     * Default constructor.
+     */
+    public TestNextOrderId() {
+    }
+
+    /**
+     * Calls "getNextOrderId", then sleeps for 10 seconds before returning in order to simulate load.
+     * @throws ServiceException if an error occurs
+     */
+    public void getNextOrderId() throws ServiceException {
+        try {
+            GetNextOrderIdService service = new GetNextOrderIdService();
+            service.setInProductStoreId(productStoreId);
+            service.setInPartyId(partyId);
+            runSync(service);
+
+            Thread.sleep(10000);
+
+            this.orderId = service.getOutOrderId();
+        } catch (Exception e) {
+            throw new ServiceException(e);
+        }
+    }
+}
