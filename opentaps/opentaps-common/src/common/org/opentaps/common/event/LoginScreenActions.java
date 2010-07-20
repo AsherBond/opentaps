7da06b5760e09d1513e5502f952d4eee2e3ecafc
si chen <sichen@opensourcestrategies.com>
2010-07-19 17:02:47 -0700

    Moved LoginScreenActions to org.opentaps.common.event

--- opentaps/opentaps-common/src/common/org/opentaps/common/event/LoginScreenActions.java
+++ opentaps/opentaps-common/src/common/org/opentaps/common/event/LoginScreenActions.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with Opentaps.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.opentaps.domain.login;
+package org.opentaps.common.event;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
