cb63bd14c2ef367f1cb048c57fa76616cae26094
Jeremy Wickersheimer <jwickers@gmail.com>
2010-07-23 11:35:43 +0800

    More reliable stopofbiz.sh script.
    Now issue a stop command every 2 seconds until the instance is really stopped.

--- stopofbiz.sh
+++ stopofbiz.sh
@@ -7,9 +7,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-#
+# 
 # http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -26,11 +26,22 @@ if [ -f "$JAVA_HOME/bin/java" ]; then
   JAVA=$JAVA_HOME/bin/java
 else
   JAVA=java
-fi
+fi                                                                                                                                                                                         
 
 # shutdown settings
 ADMIN_PORT=10523
 ADMIN_KEY=so3du5kasd5dn
 
-$JAVA -Dofbiz.admin.port=$ADMIN_PORT -Dofbiz.admin.key=$ADMIN_KEY -jar ofbiz.jar -shutdown
-
+RES=0
+FIRST=1
+echo "Stopping ofbiz"
+while [ $RES -eq 0 ]; do
+  if [ $FIRST -eq 1 ]; then
+    FIRST=0
+    $JAVA -Dofbiz.admin.port=$ADMIN_PORT -Dofbiz.admin.key=$ADMIN_KEY -jar ofbiz.jar -shutdown
+  else
+    sleep 2
+    $JAVA -Dofbiz.admin.port=$ADMIN_PORT -Dofbiz.admin.key=$ADMIN_KEY -jar ofbiz.jar -shutdown > /dev/null 2> /dev/null
+  fi
+  RES=$?
+done;
