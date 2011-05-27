34d1d6b750779b92fe7ea8ef15fbadd7a9077833
Jeremy Wickersheimer <jwickers@gmail.com>
2011-05-27 16:19:34 +0800

    Upgrade Ofbiz from 1004 rev 1104428

--- framework/entity/src/org/ofbiz/entity/GenericDelegator.java
+++ framework/entity/src/org/ofbiz/entity/GenericDelegator.java
@@ -866,7 +866,7 @@ public class GenericDelegator implements Delegator {
 
             return value;
         } catch (GenericEntityException e) {
-            String errMsg = "Failure in create operation for entity [" + value.getEntityName() + "]: " + e.toString() + ". Rolling back transaction.";
+            String errMsg = "Failure in create operation for entity [" + (value != null ? value.getEntityName() : "null")  + "]: " + e.toString() + ". Rolling back transaction.";
             Debug.logError(e, errMsg, module);
             try {
                 // only rollback the transaction if we started one...
@@ -1000,7 +1000,9 @@ public class GenericDelegator implements Delegator {
             this.saveEntitySyncRemoveInfo(primaryKey);
 
             if (testMode) {
-                storeForTestRollback(new TestOperation(OperationType.DELETE, removedEntity));
+                if (removedEntity != null) {
+                    storeForTestRollback(new TestOperation(OperationType.DELETE, removedEntity));
+                }
             }
 
             ecaRunner.evalRules(EntityEcaHandler.EV_RETURN, EntityEcaHandler.OP_REMOVE, primaryKey, false);
@@ -1066,7 +1068,9 @@ public class GenericDelegator implements Delegator {
             int num = helper.removeByPrimaryKey(value.getPrimaryKey());
 
             if (testMode) {
-                storeForTestRollback(new TestOperation(OperationType.DELETE, removedValue));
+                if (removedValue != null) {
+                    storeForTestRollback(new TestOperation(OperationType.DELETE, removedValue));
+                }
             }
 
             this.saveEntitySyncRemoveInfo(value.getPrimaryKey());
